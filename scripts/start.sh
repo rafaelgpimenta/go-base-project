b155927eab1f0a5d2eec1ed73a4343d4e0d0b72b
Rafael Pimenta <rafael.pimenta@zenvia.com>
2025-08-28 22:59:18 -0300

    chore: start app with jq

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+set -euo pipefail
+
 FOLDER="$( cd $(dirname "${BASH_SOURCE[0]}"); pwd )"
 cd $FOLDER/..
 
-go run cmd/main.go
+if command -v jq >/dev/null 2>&1; then
+  go run cmd/main.go | jq -R "fromjson? | . "
+else
+  go run cmd/main.go
+fi
